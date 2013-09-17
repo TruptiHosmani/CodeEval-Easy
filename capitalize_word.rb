444ae92d84d0e605b2cba5bbdf3fd3878b40468f
Trupti <trupti.hosamani7@gmail.com>
2013-09-17 07:31:30 -0700

    Added what input and output looks like

--- capitalize_word.rb
+++ capitalize_word.rb
@@ -1,3 +1,10 @@
+# Write a program which capitalizes words in a sentence.
+
 File.open(ARGV[0]).each_line do |line|
   puts line.split(" ").map{|i| i[0].upcase+i[1..-1]}.join(" ")
 end
+
+
+# Hello world         => Hello World
+# javaScript language => JavaScript Language
+# a letter            => A Letter
